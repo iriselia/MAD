bcba27da512e1a330f8b355d704c2952994fce9f
Xi Wen <baozi.wx@gmail.com>
2014-12-09 01:30:55 -0500

    generate javadocs; add Help screen

--- core/src/com/mygdx/game/Assets.java
+++ core/src/com/mygdx/game/Assets.java
@@ -1,8 +1,11 @@
 package com.mygdx.game;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 
 
 /**
@@ -17,6 +20,7 @@ public class Assets {
 	public static TextureAtlas menuAtlas;
 	public static TextureAtlas gameAtlas;
 	public static TextureAtlas pieceAtlas;
+	public static TextureRegionDrawable[] helpImages;
 
 	public static void loadMainMenuOrSettings() {
 		menuAtlas = new TextureAtlas("ui/atlas.pack");
@@ -26,6 +30,14 @@ public class Assets {
 	public static void loadPieces() {
 		pieceAtlas = new TextureAtlas("ui/pieces.pack");
 		pieceSkin = new Skin(Gdx.files.internal("ui/gameSkin.json"), pieceAtlas);
+	}
+	
+	public static void loadHelp() {
+		helpImages = new TextureRegionDrawable[4];
+		helpImages[0] = new TextureRegionDrawable(new TextureRegion(new Texture(Gdx.files.internal("help/1.png"))));
+		helpImages[1] = new TextureRegionDrawable(new TextureRegion(new Texture(Gdx.files.internal("help/2.png"))));
+		helpImages[2] = new TextureRegionDrawable(new TextureRegion(new Texture(Gdx.files.internal("help/3.png"))));
+		helpImages[3] = new TextureRegionDrawable(new TextureRegion(new Texture(Gdx.files.internal("help/4.png"))));
 	}
 
 	public static void disposeMainMenuOrSettings() {
